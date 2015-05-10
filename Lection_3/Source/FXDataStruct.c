468831cb57669af60554172871c09143d8944abd
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-05-10 23:12:04 +0300

    DataStruct code added

---
+++ Lection_3/Source/FXDataStruct.c
@@ -0,0 +1,9 @@
+//
+//  FXDataStruct.c
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 5/10/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Lection_3/Source/FXDataStructTests.h
@@ -0,0 +1,14 @@
+//
+//  FXDataStructTests.h
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 5/10/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FXHomeWorks_FXDataStructTests_h
+#define FXHomeWorks_FXDataStructTests_h
+
+
+
+#endif

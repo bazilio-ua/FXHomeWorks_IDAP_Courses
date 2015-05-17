eb205b5f64250eab5fa70b09656305c0c650beb4
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-05-17 14:15:56 +0300

    FXHuman.c FXHuman.h added to project

---
+++ FXHuman/Source/FXHuman/FXHuman.c
@@ -0,0 +1,9 @@
+//
+//  FXHuman.c
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 5/17/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ FXHuman/Source/FXHuman/FXHuman.h
@@ -0,0 +1,14 @@
+//
+//  FXHuman.h
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 5/17/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FXHomeWorks_FXHuman_h
+#define FXHomeWorks_FXHuman_h
+
+
+
+#endif

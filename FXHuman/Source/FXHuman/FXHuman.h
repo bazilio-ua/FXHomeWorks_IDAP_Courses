0b3dbdf2de7d6e02dbef8fd09a580a8da2ac66cd
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-05-17 22:49:00 +0300

    FXHuman.h -> accessors for FXHuman. incomplete...

--- FXHuman/Source/FXHuman/FXHuman.h
+++ FXHuman/Source/FXHuman/FXHuman.h
@@ -24,17 +24,91 @@ typedef struct FXHuman FXHuman;
 struct FXHuman {
 	FXObject _super; // inheritance from FXObject
 	
-	FXGender _gender;
 	char *_name;
 	int _age;
+	FXGender _gender;
+	
 	bool _isAlive;
 	bool _isMarried;
 	
 	FXHuman *_mother;
 	FXHuman *_father;
 	FXHuman *_spouse;
-	FXHuman **_friends;
 	FXHuman **_children;
+	FXHuman **_friends;
 };
+
+#pragma mark -
+#pragma mark Public Accessors
+
+extern
+FXHuman *FXHumanCreate(char *name, int age, FXGender gender, FXHuman *mother, FXHuman *father);
+
+extern
+void FXHumanRelease(FXHuman *human);
+
+extern
+void FXHumanSetName(FXHuman *human, char *name);
+
+extern
+char *FXHumanGetName(FXHuman *human);
+
+extern
+void FXHumanSetAge(FXHuman *human, int age);
+
+extern
+int FXHumanGetAge(FXHuman *human);
+
+extern
+void FXHumanSetGender(FXHuman *human, FXGender gender);
+
+extern
+FXGender FXHumanGetGender(FXHuman *human);
+
+extern
+void FXHumanSetIsAlive(FXHuman *human, bool alive);
+
+extern
+bool FXHumanGetIsAlive(FXHuman *human);
+
+extern
+void FXHumanSetIsMarried(FXHuman *human, bool married);
+
+extern
+bool FXHumanGetIsMarried(FXHuman *human);
+
+// spouse
+extern
+void FXHumanSetSpouse(FXHuman *human, FXHuman *spouse);
+
+extern
+FXHuman *FXHumanGetSpouse(FXHuman *human);
+
+// parents
+extern
+void FXHumanSetMother(FXHuman *human, FXHuman *mother);
+
+extern
+FXHuman *FXHumanGetMother(FXHuman *human);
+
+extern
+void FXHumanSetFather(FXHuman *human, FXHuman *father);
+
+extern
+FXHuman *FXHumanGetFather(FXHuman *human);
+
+// children
+extern
+FXHuman *FXHumanAddChild(FXHuman *human, FXHuman *spouse);
+
+extern
+int FXHumanGetChildrenCount(FXHuman *human);
+
+// friends
+extern
+FXHuman *FXHumanAddFriend(FXHuman *human, FXHuman *friend);
+
+extern
+int FXHumanGetFriendsCount(FXHuman *human);
 
 #endif

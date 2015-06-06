3f2f1cb09bf1c97a63389601666347dcd744a042
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-06-06 18:11:36 +0300

    added Tests for FXString

---
+++ FXString/Tests/FXStringTests/FXStringTests.h
@@ -0,0 +1,14 @@
+//
+//  FXStringTests.h
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 6/6/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FXHomeWorks_FXStringTests_h
+#define FXHomeWorks_FXStringTests_h
+
+
+
+#endif

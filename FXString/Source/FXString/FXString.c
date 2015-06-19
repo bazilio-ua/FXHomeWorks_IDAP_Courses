319d353b1e2bc1daa5766cd554c214ca7f69bcec
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-06-19 10:41:25 +0300

    FXString.c -> cache old data value in setter

--- FXString/Source/FXString/FXString.c
+++ FXString/Source/FXString/FXString.c
@@ -42,10 +42,11 @@ FXString *FXStringCreateWithParameters(const char *data) {
 void FXStringSetData(FXString *string, const char *data) {
 	if (NULL != string) {
 		size_t oldLength = FXStringGetLength(string);
+		char *oldData = FXStringGetData(string);
 		if (NULL != data) { // set new data value
 			size_t length = strlen(data);
 			size_t size = length * sizeof(*string->_data);
-			if (NULL == string->_data) { // if string->_data is NULL
+			if (NULL == oldData) { // if string->_data is NULL
 				string->_data = malloc(size); // we need to allocate some memory
 			} else if (length != oldLength) { // if string->_data was already set and new string has differ length
 				string->_data = realloc(string->_data, size); // or reallocate it
@@ -53,7 +54,7 @@ void FXStringSetData(FXString *string, const char *data) {
 			assert(NULL != string->_data); // make sure allocation is successfull  
 			
 			memmove(string->_data, data, length);
-		} else if (NULL != string->_data) { // if (NULL == data)
+		} else if (NULL != oldData) { // if (NULL == data)
 			free(string->_data);
 			string->_data = NULL;
 		}

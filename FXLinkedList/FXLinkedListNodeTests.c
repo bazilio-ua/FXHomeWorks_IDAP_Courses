074750d55dfaff23788fa73513f6cf4ca3976dd4
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-06-21 13:31:34 +0300

    FXLinkedListNodeTests.c -> filled up with tests

--- FXLinkedList/FXLinkedListNodeTests.c
+++ FXLinkedList/FXLinkedListNodeTests.c
@@ -7,20 +7,40 @@
 //
 
 #include <stdio.h>
+#include <assert.h>
 
 #include "FXLinkedListNodeTests.h"
+#include "FXLinkedListNode.h"
 
 void FXLinkedListNodeTests(void) {
 	//	after creation an object..
+	FXObject *object = FXObjectCreateOfType(FXObject);
+	
 	//		reference count of object must be equal to 1
-	//	after creation an node
+	assert(1 == FXObjectGetReferenceCount(object));
+	
+	//	after creation an node with object
+	FXLinkedListNode *node = FXLinkedListNodeCreateWithObject(object);
+	
 	//		reference count of node must be equal to 1
-	//	after adding object to node
+	assert(1 == FXObjectGetReferenceCount(node));
+		
 	//		reference count of object should increase and be equal to 2
-	//		reference count of node must stay unchanged and be equal to 1
+	assert(2 == FXObjectGetReferenceCount(object));
+	
+	//		object which added to node must be equal to origin object
+	assert(object == FXLinkedListNodeGetObject(node));
+	
 	//	after remove object from node
+	FXLinkedListNodeSetObject(node, NULL);
+	
 	//		his reference count should decrease and be equal to 1
+	assert(1 == FXObjectGetReferenceCount(object));
+	
 	//		reference count of node must stay unchanged and be equal to 1
+	assert(1 == FXObjectGetReferenceCount(node));
 	
 	// release them
+	FXObjectRelease(object);
+	FXObjectRelease(node);
 }

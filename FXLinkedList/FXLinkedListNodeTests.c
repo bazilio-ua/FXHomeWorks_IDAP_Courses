161a60bb934d88d5687b90825d81f64791e89bff
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-06-21 13:07:51 +0300

    FXLinkedListNodeTests.c -> user-story has added

--- FXLinkedList/FXLinkedListNodeTests.c
+++ FXLinkedList/FXLinkedListNodeTests.c
@@ -11,5 +11,16 @@
 #include "FXLinkedListNodeTests.h"
 
 void FXLinkedListNodeTests(void) {
+	//	after creation an object..
+	//		reference count of object must be equal to 1
+	//	after creation an node
+	//		reference count of node must be equal to 1
+	//	after adding object to node
+	//		reference count of object should increase and be equal to 2
+	//		reference count of node must stay unchanged and be equal to 1
+	//	after remove object from node
+	//		his reference count should decrease and be equal to 1
+	//		reference count of node must stay unchanged and be equal to 1
 	
+	// release them
 }

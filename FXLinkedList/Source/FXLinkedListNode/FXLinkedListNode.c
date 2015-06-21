ebc219f88f44cbc97a920cbbf127fc857ba7b027
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-06-21 11:46:18 +0300

    FXLinkedListNode -> carcass update

--- FXLinkedList/Source/FXLinkedListNode/FXLinkedListNode.c
+++ FXLinkedList/Source/FXLinkedListNode/FXLinkedListNode.c
@@ -7,3 +7,21 @@
 //
 
 #include <stdio.h>
+
+#include "FXLinkedListNode.h"
+
+#pragma mark -
+#pragma mark Private Declarations
+
+struct FXLinkedListNode {
+	FXObject _super; // inheritance from FXObject
+	
+	FXLinkedListNode *_nextNode;
+	void *_object; // void here is because we don't wanna to cast types to FXObject every time
+};
+
+#pragma mark -
+#pragma mark Public Methods Implementations
+
+#pragma mark -
+#pragma mark Private Accessors Implementation

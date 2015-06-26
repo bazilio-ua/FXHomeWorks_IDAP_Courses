478f875f60757765f2cdb379e4561dfba97fdf96
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-06-26 22:35:46 +0300

    FXLinkedListEnumeratorTests.c -> user story

--- FXLinkedList/Tests/FXLinkedListEnumeratorTests/FXLinkedListEnumeratorTests.c
+++ FXLinkedList/Tests/FXLinkedListEnumeratorTests/FXLinkedListEnumeratorTests.c
@@ -13,5 +13,12 @@
 #include "FXLinkedListNode.h"
 
 void FXLinkedListEnumeratorTests(void) {
-	
+	//	after creating list with 10 objects
+	//		place objects in reverse order
+	//		list reference count should be equal to 1
+	//		list count should be equal 5
+	//	after enumerator was created
+	//		list reference count should increase to 2
+	//		enumerator reference count should be 1
+	//		enumerator iterations count should be 5
 }

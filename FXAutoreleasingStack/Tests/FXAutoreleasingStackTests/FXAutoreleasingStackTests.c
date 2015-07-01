8b38879599f3ee5f2e475cc45bdecd0b269be4d7
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-07-02 00:35:23 +0300

    FXAutoreleasingStackTests.c -> extend tests

--- FXAutoreleasingStack/Tests/FXAutoreleasingStackTests/FXAutoreleasingStackTests.c
+++ FXAutoreleasingStack/Tests/FXAutoreleasingStackTests/FXAutoreleasingStackTests.c
@@ -80,41 +80,108 @@ void FXAutoreleasingStackOneObjectPushTests(void) {
 	//			stack must be not full
 	assert(false == FXAutoreleasingStackIsFull(stack));
 	
-	// release it
+	// release them
 	FXObjectRelease(object);
 	FXObjectRelease(stack);
 }
 
 void FXAutoreleasingStackOneNULLPushTest(void) {
-	//	after stack was created for 64 objects
+	//	after stack was created with size 512 (64 *pointers on it)
+	FXAutoreleasingStack *stack = FXAutoreleasingStackCreateWithSize(64 * sizeof(FXObject *));
+	
 	//		stack must be empty
+	assert(true == FXAutoreleasingStackIsEmpty(stack));
+	
 	//		stack must be not full
+	assert(false == FXAutoreleasingStackIsFull(stack));
+	
 	//	after NULL pushed to the stack
+	FXAutoreleasingStackPushObject(stack, NULL);
+	
 	//		stack must be not empty
+	assert(false == FXAutoreleasingStackIsEmpty(stack));
+	
 	//		stack must be not full
+	assert(false == FXAutoreleasingStackIsFull(stack));
+	
 	//	after one object was popped
+	FXAutoreleasingStackPopType type = FXAutoreleasingStackPopObject(stack);
+	
 	//		pop type must be 'poppedNULL'
+	assert(kFXAutoreleasingStackPoppedNULL == type);
+	
 	//		stack must be empty
+	assert(true == FXAutoreleasingStackIsEmpty(stack));
+	
 	//		stack must be not full
+	assert(false == FXAutoreleasingStackIsFull(stack));
+	
+	// release stack
+	FXObjectRelease(stack);
 }
 
 void FXAutoreleasingStackIsFullTest(void) {
 	//	after stack was created for 64 objects
+	FXAutoreleasingStack *stack = FXAutoreleasingStackCreateWithSize(64 * sizeof(FXObject *));
+	
 	//		stack must be empty
+	assert(true == FXAutoreleasingStackIsEmpty(stack));
+	
 	//		stack must be not full
+	assert(false == FXAutoreleasingStackIsFull(stack));
+	
 	//	after object was created
-	//		after object pushed to stack 64 times
-	//		object reference count must not change
+	FXObject *object = FXObjectCreateOfType(FXObject);
+	
+	//		object reference count must be equal to 1
+	assert(1 == FXObjectGetReferenceCount(object));
+	
+	//		after object was retained and pushed to stack 64 times
+	for (uint64_t counter = 0; counter < 64; counter++) {
+		FXObjectRetain(object);
+		FXAutoreleasingStackPushObject(stack, object);
+	}
+	
+	//		object reference count must be equal 65
+	assert(65 == FXObjectGetReferenceCount(object));
+	
 	//		stack must be not empty
+	assert(false == FXAutoreleasingStackIsEmpty(stack));
+	
 	//		stack must be full
+	assert(true == FXAutoreleasingStackIsFull(stack));
+	
 	//	after one object was popped
+	FXAutoreleasingStackPopType type = FXAutoreleasingStackPopObject(stack);
+	
 	//		pop type must be 'poppedObject'
-	//		object reference count must decrement
+	assert(kFXAutoreleasingStackPoppedObject == type);
+	
+	//		object reference count must decrement by 1
+	assert(64 == FXObjectGetReferenceCount(object));
+	
 	//		stack must be not empty
+	assert(false == FXAutoreleasingStackIsEmpty(stack));
+	
 	//		stack must be not full
+	assert(false == FXAutoreleasingStackIsFull(stack));
+	
 	//	after all object was popped
+	type = FXAutoreleasingStackPopObjectsUntilNULL(stack);
+	
 	//		pop type must be 'poppedObject'
+	assert(kFXAutoreleasingStackPoppedObject == type);
+	
 	//		object reference count must be decremented 63 times
+	assert(1 == FXObjectGetReferenceCount(object));
+	
 	//		stack must be empty
+	assert(true == FXAutoreleasingStackIsEmpty(stack));
+	
 	//		stack must be not full
+	assert(false == FXAutoreleasingStackIsFull(stack));
+	
+	// release them
+	FXObjectRelease(object);
+	FXObjectRelease(stack);
 }

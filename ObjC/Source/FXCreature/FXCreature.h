7f22f287cafae04e9e79ceaed3773e419be72c59
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-07-07 22:57:07 +0300

    FXCreature -> wireframe

--- ObjC/Source/FXCreature/FXCreature.h
+++ ObjC/Source/FXCreature/FXCreature.h
@@ -8,6 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+	kFXCreatureGenderMale = 1, // don't use Undefined
+	kFXCreatureGenderFemale
+} FXCreatureGender;
+
 @interface FXCreature : NSObject
+
+@property (nonatomic, copy)		NSString	*name;
+@property (nonatomic, retain)	NSArray		*children;
+
+@property (nonatomic, assign)	float		weight;
+@property (nonatomic, assign)	uint32_t	age;
+@property (nonatomic, assign)	FXCreatureGender	gender;
+
+- (instancetype)init;
+- (instancetype)initWithName:(NSString *)aName age:(uint32_t)aAge gender:(FXCreatureGender)aGender;
+
+- (void)addChild:(FXCreature *)aChild;
+- (void)removeChild:(FXCreature *)aChild;
+
+// merge this to one method 'doMainJob'
+- (void)goToBattle;
+- (FXCreature *)giveBirth;
+
+- (void)sayHello;
 
 @end
--- ObjC/Source/FXCreature/FXCreature.m
+++ ObjC/Source/FXCreature/FXCreature.m
@@ -10,4 +10,43 @@
 
 @implementation FXCreature
 
+#pragma mark -
+#pragma mark  Initializations and Deallocations
+
+- (void)dealloc {
+	self.name = nil;
+	self.children = nil;
+	
+	[super dealloc];
+}
+
+
+- (instancetype)init {
+	return self;
+}
+
+- (instancetype)initWithName:(NSString *)name age:(uint32_t)age gender:(FXCreatureGender)gender {
+	return self;
+}
+
+- (void)addChild:(FXCreature *)child {
+	
+}
+- (void)removeChild:(FXCreature *)child {
+	
+}
+
+// merge this to one method 'doMainJob'
+- (void)goToBattle {
+	
+}
+- (FXCreature *)giveBirth {
+	return self;
+}
+
+- (void)sayHello {
+	
+}
+
+
 @end

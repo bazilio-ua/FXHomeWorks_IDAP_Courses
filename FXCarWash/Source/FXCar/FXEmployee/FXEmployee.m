993fb7a5efbae6a1f6e89257f7da226185c62dc9
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-07-19 13:59:32 +0300

    FXHuman -> FXEmployee

---
+++ FXCarWash/Source/FXCar/FXEmployee/FXEmployee.m
@@ -0,0 +1,13 @@
+//
+//  FXEmployee.m
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 7/17/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import "FXEmployee.h"
+
+@implementation FXEmployee
+
+@end

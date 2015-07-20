ee312b4d9c7622299a0ad8cec49e5f6bf9c9e85c
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-07-20 23:25:23 +0300

    carWash -> add moneyFlow protocol file

---
+++ FXCarWash/Source/FXCar/FXEmployee/FXWorkFlow/FXMoneyFlow/FXMoneyFlow.h
@@ -0,0 +1,13 @@
+//
+//  FXMoneyFlow.h
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 7/20/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FXMoneyFlow <NSObject>
+
+@end

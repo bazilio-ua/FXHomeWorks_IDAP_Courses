46762ec8a43c361f0c24e302cbe6646f625adb96
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-08-02 10:36:56 +0300

    FXAlphabet -> first iterate

--- FXAlphabet/Source/FXAlphabet/FXAlphabet.h
+++ FXAlphabet/Source/FXAlphabet/FXAlphabet.h
@@ -10,4 +10,18 @@
 
 @interface FXAlphabet : NSObject
 
++ (id)alphabetWithSymbols:(NSString *)symbols;
++ (id)alphabetWithStrings:(NSArray *)strings;
++ (id)alphabetWithRange:(NSRange)range;
++ (id)alphabetWithAlphabets:(NSArray *)alphabets;
+
+- (id)initWithSymbols:(NSString *)symbols;
+- (id)initWithStrings:(NSArray *)strings;
+- (id)initWithRange:(NSRange)range;
+- (id)initWithAlphabets:(NSArray *)alphabets;
+
+- (NSUInteger)count;
+
+- (NSString *)stringAtIndex:(NSUInteger)index;
+
 @end
--- FXAlphabet/Source/FXAlphabet/FXAlphabet.m
+++ FXAlphabet/Source/FXAlphabet/FXAlphabet.m
@@ -10,4 +10,44 @@
 
 @implementation FXAlphabet
 
+#pragma mark -
+#pragma mark Class Methods
+
++ (id)alphabetWithSymbols:(NSString *)symbols {
+	return nil;
+}
++ (id)alphabetWithStrings:(NSArray *)strings {
+	return nil;
+}
++ (id)alphabetWithRange:(NSRange)range {
+	return nil;
+}
++ (id)alphabetWithAlphabets:(NSArray *)alphabets {
+	return nil;
+}
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (id)initWithSymbols:(NSString *)symbols {
+	return nil;
+}
+- (id)initWithStrings:(NSArray *)strings {
+	return nil;
+}
+- (id)initWithRange:(NSRange)range {
+	return nil;
+}
+- (id)initWithAlphabets:(NSArray *)alphabets {
+	return nil;
+}
+
+- (NSUInteger)count {
+	return 0;
+}
+
+- (NSString *)stringAtIndex:(NSUInteger)index {
+	return nil;
+}
+
 @end

f56c68c810b79f28dcc955138a3adf6e6dfbe1cd
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-08-19 20:27:41 +0300

    add NSArray+FXExtensions category for 'firstObj'

---
+++ FXExtensions/NSArray+FXExtensions.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+FXExtensions.h
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 8/19/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (FXExtensions)
+
+- (id)firstObj;
+
+@end

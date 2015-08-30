9b7673833e03de5701473c489832d1c83af73ddd
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-08-30 18:02:07 +0300

    FXDispatcher -> initial commit

---
+++ FXCarWash/Source/FXDispatcher/FXDispatcher.h
@@ -0,0 +1,13 @@
+//
+//  FXDispatcher.h
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 8/30/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FXDispatcher : NSObject
+
+@end
---
+++ FXCarWash/Source/FXDispatcher/FXDispatcher.m
@@ -0,0 +1,13 @@
+//
+//  FXDispatcher.m
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 8/30/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import "FXDispatcher.h"
+
+@implementation FXDispatcher
+
+@end

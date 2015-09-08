22c0969b86d24aa65e2a34c4a992ce3d632848ca
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-09-08 22:13:25 +0300

    re-arranged project and add UIKit target

---
+++ UIKit/Source/FXViewController/FXViewController.h
@@ -0,0 +1,13 @@
+//
+//  FXViewController.h
+//  UIKit
+//
+//  Created by Basil Nikityuk on 9/8/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FXViewController : UIViewController
+
+@end
---
+++ UIKit/UIKit/FXAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FXAppDelegate.h
+//  UIKit
+//
+//  Created by Basil Nikityuk on 9/8/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FXViewController;
+
+@interface FXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FXViewController *viewController;
+
+@end

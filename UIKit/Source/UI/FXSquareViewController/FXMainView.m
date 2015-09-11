2b56ce8f9d4ae8b92792f345f1e7b30e5eb2b330
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-09-11 15:14:06 +0300

    model is not bound

--- UIKit/Source/UI/FXSquareViewController/FXMainView.m
+++ UIKit/Source/UI/FXSquareViewController/FXMainView.m
@@ -8,7 +8,11 @@
 
 #import "FXMainView.h"
 
+#import "FXSquareView.h"
 #import "FXSquareModel.h"
+
+static const NSTimeInterval kFXSquareViewAnimationDuration = 0.5;
+static const NSTimeInterval kFXSquareViewAnimationDelay = 0;
 
 @interface FXMainView ()
 
@@ -51,14 +55,55 @@
 - (void)setSquarePosition:(FXSquarePosition)position 
 				 animated:(BOOL)animated 
 		 completionHanler:(void (^)(BOOL finished))completion {
+	NSTimeInterval duration = animated ? kFXSquareViewAnimationDuration : 0;
 	
+	[UIView animateWithDuration:duration 
+						  delay:kFXSquareViewAnimationDelay 
+						options:UIViewAnimationOptionBeginFromCurrentState 
+					 animations:^{
+						 self.squareView.frame = [self frameForSquarePosition:position];
+					 } 
+					 completion:^(BOOL finished) {
+						 self.squareView.squareModel.squarePosition = position;
+						 
+						 if (completion) {
+							 completion(finished);
+						 }
+					 }];
 }
 
 #pragma mark -
 #pragma mark Private Methods
 
 - (CGRect)frameForSquarePosition:(FXSquarePosition)position {
-	return CGRectNull;
+	CGRect frame = self.squareView.frame;
+	CGRect bounds = self.superview.bounds;
+	CGPoint point = CGPointZero;
+	
+	switch (position) {
+		case kFXSquarePositionTopLeft:
+			break;
+			
+		case kFXSquarePositionTopRight:
+			point.x = CGRectGetWidth(bounds) - CGRectGetWidth(frame);
+			break;
+			
+		case kFXSquarePositionBottomRight:
+			point.x = CGRectGetWidth(bounds) - CGRectGetWidth(frame);
+			point.y = CGRectGetHeight(bounds) - CGRectGetHeight(frame);
+			break;
+			
+		case kFXSquarePositionBottomLeft:
+			point.y = CGRectGetHeight(bounds) - CGRectGetHeight(frame);
+			break;
+			
+		default:
+			break;
+	}
+	
+	frame.origin = point;
+	
+	return frame;
 }
 
 @end

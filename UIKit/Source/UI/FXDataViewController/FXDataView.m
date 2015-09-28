9779b02e7fd1a0536def24eb6cc5200b480e8155
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-09-28 21:49:10 +0300

    editButton title updating

--- UIKit/Source/UI/FXDataViewController/FXDataView.m
+++ UIKit/Source/UI/FXDataViewController/FXDataView.m
@@ -8,6 +8,15 @@
 
 #import "FXDataView.h"
 
+static NSString * const kFXEditButtonTitleEdit 	= @"Edit";
+static NSString * const kFXEditButtonTitleDone 	= @"Done";
+
+@interface FXDataView ()
+
+- (void)updateEditButtonTitle;
+
+@end
+
 @implementation FXDataView
 
 @synthesize tableView		= _tableView;
@@ -22,10 +31,21 @@
 
 - (void)setEditing:(BOOL)editing {
 	[self.tableView setEditing:editing animated:YES];
+	
+	[self updateEditButtonTitle];
 }
 
 - (BOOL)isEditing {
 	return self.tableView.editing;
+}
+
+#pragma mark -
+#pragma mark Private Methods
+
+- (void)updateEditButtonTitle {
+	NSString *title = [self isEditing] ? kFXEditButtonTitleDone : kFXEditButtonTitleEdit;
+	[self.editButton setTitle:title
+					 forState:UIControlStateNormal];
 }
 
 @end

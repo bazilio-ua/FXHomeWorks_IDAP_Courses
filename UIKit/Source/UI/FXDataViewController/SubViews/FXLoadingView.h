f5415bc7be8c588171170ca25b4a988af38bab15
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-10-12 20:43:01 +0300

    FXLoadingView initial commit

---
+++ UIKit/Source/UI/FXDataViewController/SubViews/FXLoadingView.h
@@ -0,0 +1,13 @@
+//
+//  FXLoadingView.h
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 10/12/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FXLoadingView : UIView
+
+@end
---
+++ UIKit/Source/UI/FXDataViewController/SubViews/FXLoadingView.m
@@ -0,0 +1,13 @@
+//
+//  FXLoadingView.m
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 10/12/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import "FXLoadingView.h"
+
+@implementation FXLoadingView
+
+@end

2b573cdbf027f00eef8d12a6ba59f963ca61c5be
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-10-21 21:34:32 +0300

    FXImageView -> initial commit

---
+++ UIKit/Source/UI/FXDataViewController/SubViews/FXImageView.h
@@ -0,0 +1,13 @@
+//
+//  FXImageView.h
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 10/21/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import "FXView.h"
+
+@interface FXImageView : FXView
+
+@end
---
+++ UIKit/Source/UI/FXDataViewController/SubViews/FXImageView.m
@@ -0,0 +1,13 @@
+//
+//  FXImageView.m
+//  FXHomeWorks
+//
+//  Created by Basil Nikityuk on 10/21/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import "FXImageView.h"
+
+@implementation FXImageView
+
+@end

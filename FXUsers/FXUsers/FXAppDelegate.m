385283ce9486b2cfbaa4580ddbcd1d3f219bf79b
Basil Nikityuk <basil.nikityuk@gmail.com>
2015-10-31 20:54:49 +0200

    FXUsers fb app -- initial commit

---
+++ FXUsers/FXUsers/FXAppDelegate.m
@@ -0,0 +1,43 @@
+//
+//  FXAppDelegate.m
+//  FXUsers
+//
+//  Created by Basil Nikityuk on 10/31/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import "FXAppDelegate.h"
+
+@implementation FXAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    
+}
+
+@end
